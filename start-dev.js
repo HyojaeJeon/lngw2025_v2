e358e0d2a4209ef5f21de353ca309fb9ed3587df
HyojaeJeon <87747771+HyojaeJeon@users.noreply.github.com>
2025-06-08 07:07:54 +0000

    123

--- start-dev.js
+++ start-dev.js
@@ -1,27 +1,31 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process');
-const path = require('path');
+const { spawn } = require("child_process");
+const path = require("path");
 
 // Start Next.js client
-const clientProcess = spawn('npx', ['next', 'dev', '-p', '3000', '--hostname', '0.0.0.0'], {
-  cwd: path.join(__dirname, 'client-nextjs'),
-  stdio: 'inherit'
-});
+const clientProcess = spawn(
+  "npx",
+  ["next", "dev", "-p", "3000", "--hostname", "0.0.0.0"],
+  {
+    cwd: path.join(__dirname, "client-nextjs"),
+    stdio: "inherit",
+  },
+);
 
 // Start server
-const serverProcess = spawn('node', ['index.js'], {
-  cwd: path.join(__dirname, 'server'),
-  stdio: 'inherit'
+const serverProcess = spawn("node", ["index.js"], {
+  cwd: path.join(__dirname, "server"),
+  stdio: "inherit",
 });
 
 // Handle process cleanup
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   clientProcess.kill();
   serverProcess.kill();
   process.exit();
 });
 
-console.log('Starting LN Partners Groupware...');
-console.log('Client: http://localhost:3000');
-console.log('Server: http://localhost:3001');
+console.log("Starting LN Partners Groupware...");
+console.log("Client: http://localhost:3000");
+console.log("Server: http://localhost:3001");
