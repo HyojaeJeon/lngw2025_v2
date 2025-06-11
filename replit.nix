4d3a70b795919f5d42c9beae715907adcc059641
HyojaeJeon <87747771+HyojaeJeon@users.noreply.github.com>
2025-06-11 09:32:42 +0000

    Pre-Recovery checkpoint

--- replit.nix
+++ replit.nix
@@ -1,12 +1,8 @@
 { pkgs }: {
   deps = [
-    pkgs.systemdMinimal
-    pkgs.nano
-    pkgs.haskellPackages.snap-templates
-    pkgs.python312Packages.pyngrok
-    pkgs.unzipNLS
-    pkgs.wget
-    pkgs.nodejs    # 이미 깔려 있을 수도 있지만 명시적으로 적어주세요
-    pkgs.mysql     # MySQL 서버 데몬 설치
+    pkgs.nodejs-20_x
+    pkgs.bashInteractive
+    pkgs.lsof
+    pkgs.nix-output-monitor
   ];
 }
